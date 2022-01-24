292960efc500f5bfa20d19e68422aaa8f24ec1ab
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-01-24 07:30:25 +0200

    Added Rating Enum

--- src/zw/co/afrosoft/app/data/Product.java
+++ src/zw/co/afrosoft/app/data/Product.java
@@ -3,36 +3,62 @@ package zw.co.afrosoft.app.data;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import static zw.co.afrosoft.app.data.Rating.*;
+
 public class Product {
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product() {
+        this(0,"no name",BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id,name,price,NOT_RATED);
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
+//    public void setPrice(final BigDecimal price) {
+//        this.price = price;
+//    }
     public BigDecimal getDiscount(){
         return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public Product applyRating(Rating newRating){
+        return new Product(id,name,price,newRating);
     }
 }
