3b39779bc08a3503156baa1565409d0f7068e49b
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-01-27 21:14:17 +0200

    Implemented Collections API to replace Arrays

--- src/zw/co/afrosoft/app/Shop.java
+++ src/zw/co/afrosoft/app/Shop.java
@@ -14,16 +14,26 @@ public class Shop {
         ProductManager pm = new ProductManager(Locale.US);
 
         Product p1 = pm.createProduct(101,"Tea",BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-        pm.printProductReport();
+        pm.printProductReport(p1);
         p1 = pm.reviewProduct(p1,Rating.FOUR_STAR,"Nice hot cup of tea");
         p1 = pm.reviewProduct(p1,Rating.TWO_STAR,"Rather weak tea");
         p1 = pm.reviewProduct(p1,Rating.THREE_STAR,"Good tea");
         p1 = pm.reviewProduct(p1,Rating.ONE_STAR,"Hot cup of tea");
         p1 = pm.reviewProduct(p1,Rating.FIVE_STAR,"Just regular tea");
         p1 = pm.reviewProduct(p1,Rating.FOUR_STAR,"Not that hot though");
-        pm.printProductReport();
-//        Product p2 = pm.createProduct(102,"Coffie",BigDecimal.valueOf(2.99), Rating.FOUR_STAR);
-//        Product p3 = pm.createProduct(103,"Cake",BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+        pm.printProductReport(p1);
+
+        Product p2 = pm.createProduct(102,"Coffie",BigDecimal.valueOf(2.99), Rating.FOUR_STAR);
+        p2 = pm.reviewProduct(p2,Rating.THREE_STAR,"Good Coffie");
+        p2 = pm.reviewProduct(p2,Rating.ONE_STAR,"Hot cup of Coffie");
+        p2 = pm.reviewProduct(p2,Rating.FIVE_STAR,"Just regular Coffie");
+        pm.printProductReport(p2);
+        Product p3 = pm.createProduct(103,"Cake",BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
+        p3 = pm.reviewProduct(p3,Rating.THREE_STAR,"Good ");
+        p3 = pm.reviewProduct(p3,Rating.ONE_STAR,"Sweet Cake");
+        p3 = pm.reviewProduct(p3,Rating.FIVE_STAR,"Yummy");
+        p3 = pm.reviewProduct(p3,Rating.FOUR_STAR,"Not fresh");
+        pm.printProductReport(p3);
 //        Product p4 = pm.createProduct(105,"Cookie",BigDecimal.valueOf(3.99),Rating.TWO_STAR,LocalDate.now());
 //        Product p5 = p3.applyRating(Rating.THREE_STAR);
 //
